e902e237c636710d44bb833e6d9e36b179342d40
Daniel <daniel.bnu7@gmail.com>
2019-07-10 21:43:39 -0300

    Array simples treino

---
+++ ARRAY2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region ARRAY SIMPLES DOUBLE COM SOMA E MEDIA
+
+            // Double[] notas = new double[5];
+
+            // notas[0] = 6;
+            // notas[1] = 7;
+            // notas[2] = 8;
+            // notas[3] = 9;
+            // notas[4] = 10;
+
+            // double soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+            //
+            // double media = soma / 5;
+
+            // double[] notas = { 3, 5, 6, 7, 8, 9 };
+            //
+            // double soma = notas.Sum();
+            // double media = notas.Average();
+            //
+            // Console.WriteLine("A soma das notas é " + soma);
+            // Console.WriteLine("A media das notas é " + media);
+            //
+            // 
+            // Console.ReadKey();
+
+            #endregion
+
+            #region MATRIZ BIDIMENCIONAL IZI
+
+            //  String[,] matriz = new string[2,2];
+            //
+            //  matriz[0, 0] = "Linha 0 - COluna 0 - DANIEL";
+            //  matriz[1, 0] = "Linha 1 - COluna 0 - TAFF " ;
+            //
+            //
+            //  matriz[0, 1] = "Linha 0 - COluna 1 - 9";
+            //  matriz[1, 1] = "Linha 1 - COluna 1 - 10 ";
+            //
+            //  Console.WriteLine(matriz[0, 0]);
+            //  Console.WriteLine(matriz[1, 1]);
+
+            #endregion
+
+
+
+            Console.ReadKey();
+
+          
+        }
+    }
+}
