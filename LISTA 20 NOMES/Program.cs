9b36aa97923e7f82064850037fac38ff8701ae9e
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 15:39:26 -0300

    liSTA 20 NOMES FEITA

--- LISTA 20 NOMES/Program.cs
+++ LISTA 20 NOMES/Program.cs
@@ -8,10 +8,34 @@ namespace LISTA_20_NOMES
 {
     class Program
     {
+        static string[,] alunos;
         static void Main(string[] args)
         {
+            CarregaBaseDeAlunos();
 
-            string[,] alunos = new string[20, 3] { { "Aluno01", "19", "M" },{ "Aluno02", "45", "F" },
+            Console.WriteLine("LISTA DE ALUNOS");
+            Console.WriteLine("");
+            Console.WriteLine("DIGITE (1) PARA LISTAR ALUNOS E (2) PARA SAIR DO SISTEMA");
+            Console.WriteLine("");
+
+            int.TryParse(Console.ReadKey().KeyChar.ToString(), out int opcao);
+
+            if (opcao == 1)
+            {
+                MostarLIsta(); 
+            }
+
+            if (opcao == 2)
+
+            {
+                return;
+            }
+        }
+
+        public static void CarregaBaseDeAlunos()
+
+        {
+             alunos = new string[20, 3] { { "Aluno01", "19", "M" },{ "Aluno02", "45", "F" },
                                                    { "Aluno03", "22", "F" },{ "Aluno04", "11", "M" },
                                                    { "Aluno05", "31", "M" },{ "Aluno06", "33", "M" },
                                                    { "Aluno07", "44", "F" },{ "Aluno08", "23", "F" },
@@ -22,14 +46,20 @@ namespace LISTA_20_NOMES
                                                    { "Aluno17", "55", "M" },{ "Aluno18", "17", "F" },
                                                    { "Aluno19", "12", "M"},{ "Aluno20", "23", "M" } };
 
-            for (int i = 0; i < alunos.GetLength(0); i++)
+        }
 
+        public static void MostarLIsta()
 
-                Console.WriteLine($"O Aluno: {alunos[i, 0]}" +
-                      $" tem a idade: {alunos[i, 1]}" +
-                      $" é do sexo: {alunos[i, 2]}");
+        {
 
-            Console.ReadKey();
+           for (int i = 0; i < alunos.GetLength(0); i++)
+         
+         
+               Console.WriteLine($"O Aluno: {alunos[i, 0]}" +
+                     $" tem a idade: {alunos[i, 1]}" +
+                     $" é do sexo: {alunos[i, 2]}");
+         
+           Console.ReadKey();
 
         }
 
