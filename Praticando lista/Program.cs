4e508ec28cc77463611964285fa673b2d68ade62
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-15 14:05:44 -0300

    ATIVIDADE CRIAR LISTA FEITA 15/07
    COM AumentaALista

---
+++ Praticando lista/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praticando_lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeCarros = new string[3, 3];
+            int IDLista = 0;
+
+            for (int i = 0; i < listaDeCarros.GetLength(0); i++)
+            {
+
+                if (listaDeCarros[i, 0] != null)
+                    continue;
+
+                Console.WriteLine("informe um carro para adicionar na lista");
+                var nomeCarro = Console.ReadLine();
+                
+
+                listaDeCarros[i, 0] = (IDLista++).ToString();
+                listaDeCarros[i, 1] = nomeCarro;
+
+                Console.WriteLine("informe a placa do carro para adicionar na lista");
+                var placa = Console.ReadLine();
+
+                listaDeCarros[i, 2] = placa;
+
+                Console.WriteLine("Deseja inserir um novo registro? sim(1) Não(0)");
+                var continuar = Console.ReadKey().KeyChar.ToString();
+                Console.Clear();
+
+
+                if (continuar == "0")
+                    break;
+
+              
+
+                AumentaALista(ref listaDeCarros);
+            }
+
+            Console.WriteLine("Registro adicionado com sucesso, lista das informações adicionadas: ");
+           
+            for (int i = 0; i < listaDeCarros.GetLength(0); i++)
+                //Utilizamos o string format, basicamente ele faz da mesma forma que o $
+                //a diferença entre eles é que este e um cara em grande quantidades
+                //acaba sendo mais rápido
+
+                Console.WriteLine(string.Format("Registro ID {0} - Carro: {1} - Placa: {2} ", listaDeCarros[i, 0], listaDeCarros[i, 1], listaDeCarros[i,2]));
+
+                Console.ReadKey();
+
+        }
+
+        public static void AumentaALista(ref string[,] listaDeCarros)
+
+        {
+            var limiteDaLista = true;
+
+            for (int i = 0; i < listaDeCarros.GetLength(0); i++)
+
+
+            {
+
+                if (listaDeCarros[i, 0] == null)
+                    limiteDaLista = false;
+
+            }
+
+            if (limiteDaLista)
+            {
+                var listaCopia = listaDeCarros;
+
+                listaDeCarros = new string[listaDeCarros.GetLength(0) + 5, 3];
+
+                for (int i = 0; i < listaCopia.GetLength(0); i++)
+                {
+                    listaDeCarros[i, 0] = listaCopia[i, 0];
+                    listaDeCarros[i, 1] = listaCopia[i, 1];
+                    listaDeCarros[i, 2] = listaCopia[i, 2];
+                }
+
+                Console.WriteLine("o tamanho da lista foi att");
+
+            }
+
+
+        }
+
+
+    }
+}
