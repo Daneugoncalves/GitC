d4b50e5c14787b7d28abbf408bbfdb801ef83adc
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-17 11:14:34 -0300

    insert e comitei

---
+++ Querrys/MyfirtInsert.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO AulaCshap(Aluno,Idade,Ativo,UsuInc,UsuAlt,DatInc,DatAlt)
+VALUES ('Daniel',20,1,321,321,getdate(),getdate()),
+	   ('Carou',13,1,321,321,getdate(),getdate()),
+	   ('Ronaldo',19,1,321,321,getdate(),getdate()),
+	   ('Carlinho',39,1,321,321,getdate(),getdate()),
+	   ('Rogerio',10,1,321,321,getdate(),getdate());
