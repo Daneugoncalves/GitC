ae4e20f78d43c0a0295ce8ebcacc80ddc49fc1a4
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:08:11 -0300

    Food truck doguinho sql

---
+++ Querrys/SelectFoodtruckCompleto.sql
@@ -0,0 +1,10 @@
+﻿select 
+TipoDog,
+SUM(Vendas) as 'Total de vendas',
+MONTH (DataVenda) as 'Mês'
+
+from FoodTruck
+
+where Id Between 2 and 7
+group by TipoDog,MONTH(DataVenda)
+order by 'Total de vendas' desc
