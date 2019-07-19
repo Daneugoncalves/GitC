14a6be373877441dfcb60f1e8cb4b46fff8a667f
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-19 17:02:12 -0300

    COMIT INSERTS

---
+++ ExerciciosBancoDeDados/SCRIP TB TURMA.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[Turma] ON
+INSERT INTO [dbo].[Turma] ([Id], [Turma], [Descrição]) VALUES (1, N'101', N'C#')
+INSERT INTO [dbo].[Turma] ([Id], [Turma], [Descrição]) VALUES (3, N'102', N'java')
+SET IDENTITY_INSERT [dbo].[Turma] OFF
---
+++ ExerciciosBancoDeDados/SCRIPT TABELA dbo.Aluno.data.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [dbo].[Aluno] ON
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Matricula], [Frequencia]) VALUES (4, N'Daniel', 1234, 1)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Matricula], [Frequencia]) VALUES (5, N'Carou', 1235, 0)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Matricula], [Frequencia]) VALUES (6, N'gabriel', 3124, 1)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Matricula], [Frequencia]) VALUES (7, N'Ronaldo', 23213, 1)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Matricula], [Frequencia]) VALUES (8, N'gilso', 39432, 0)
+INSERT INTO [dbo].[Aluno] ([Id], [Nome], [Matricula], [Frequencia]) VALUES (9, N'Roberta', 231222, 1)
+SET IDENTITY_INSERT [dbo].[Aluno] OFF
