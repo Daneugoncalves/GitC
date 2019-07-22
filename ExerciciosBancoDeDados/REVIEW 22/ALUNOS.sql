7c882409ddec9252e0b6b38ad3729f05d5eca785
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 10:59:27 -0300

    COMIT SELECT REVIEW

---
+++ ExerciciosBancoDeDados/REVIEW 22/ALUNOS.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Alunos]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Aluno] VARCHAR(110) NOT NULL,
+    [Ativo] bit NOT NULL,
+    [UsuInc] int not null,
+	[UsuAlt] int not null,
+	DataInc datetime not null default getdate(), 
+	DataAlt datetime not null default getdate(), 
+    CONSTRAINT [FK_Alunos_Usuarios_Inc] FOREIGN KEY ([UsuInc]) REFERENCES [Usuarios]([Id]),
+    CONSTRAINT [FK_Alunos_Usuarios_Alt] FOREIGN KEY ([UsuAlt]) REFERENCES [Usuarios]([Id]),
+)
---
+++ ExerciciosBancoDeDados/REVIEW 22/DIARIO.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Diarios]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+	[Data] DATETIME NOT NULL DEFAULT GETDATE(),
+	[Turma] int NOT NULL,
+	[Obs] VARCHAR (500) NOT NULL,
+	[Ativo] BIT NOT NULL,
+	[UsuInc] int not null,
+	[UsuAlt] int not null,
+	DataInc datetime not null default getdate(), 
+	DataAlt datetime not null default getdate(), 
+    CONSTRAINT [FK_Diarios_Turma] FOREIGN KEY ([Turma]) REFERENCES [Turmas]([Id]),
+	CONSTRAINT [FK_Diarios_Usuarios_Inc] FOREIGN KEY ([UsuInc]) REFERENCES [Usuarios]([Id]),
+    CONSTRAINT [FK_Diarios_Usuarios_Alt] FOREIGN KEY ([UsuAlt]) REFERENCES [Usuarios]([Id]),
+)
