130ed685cf87e489b91b6c64eaf4167daa000cd2
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-22 13:34:12 -0300

    comit meio dia selects

---
+++ ExerciciosBancoDeDados/REVIEW 22/SELECT LOKO CONTINUACAO.sql
@@ -0,0 +1,24 @@
+﻿Select tr.Nome as 'Turma', 
+		a.Aluno as 'Alunos'
+		
+
+from turmas tr inner join TurmaAlunos ta on tr.Id = ta.Turma
+inner join Alunos a on ta.Aluno=a.Id
+left join Diario d on tr.Id = d.Turma;
+
+select 
+		d.[Data],
+		tr.Aluno,
+		--a.Aluno,
+		ISNULL(n.Nota,0) as 'Nota' ,
+		IIF(ISNULL(p.Id,0) >0 ,'Presente', 'Ausente') as 'Presenças'
+		
+	from Diario d
+
+	inner join TurmaAlunos tr on d.Turma = tr.Turma
+	--inner join Turmas t on d.Turma = t.Id
+	--inner join Alunos a on tr.Aluno = a.Id
+	left join Presenca P on d.Id = p.Diario and tr.Aluno = p.Aluno
+	left join Nota n on tr.Aluno = n.Aluno and d.Id = n.Diario
+
+
