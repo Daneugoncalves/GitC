f5f38a1e29d466dff572d176c75aff344e6ec29d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 15:45:10 -0300

    Biblioteca externa puxa documentos OS

---
+++ MyFirstBibliotecaExterna/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaDeArquivosDoWindows ;
+
+namespace MyFirstBibliotecaExterna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var teste = new GetFiles();
+
+            var listaDocuments = teste.RetornaArquivosDoMeuDocumentos();
+
+            for (int i = 0; i < listaDocuments.Length; i++)
+
+            {
+                Console.WriteLine(listaDocuments[i]);
+                Console.ReadKey();
+            }
+
+
+            
+            var ListaGit = teste.RetornaArquivosGit();
+          
+            for (int i = 0; i < ListaGit.Length; i++)
+          
+            {
+                    Console.WriteLine(ListaGit[i]);
+                    Console.ReadKey();
+             
+            }
+
+
+            var listaDePictures = teste.RetornaArquivosImagesFiles();
+
+            for (int i = 0; i < listaDePictures.Length; i++)
+
+            {
+                Console.WriteLine(listaDePictures[i]);
+                Console.ReadKey();
+            }
+        }
+
+ 
+    }
+}
