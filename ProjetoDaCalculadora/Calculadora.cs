485c25a7173fb411f48382faba8167e306dd7d24
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:26:21 -0300

    calcQuase

---
+++ ProjetoDaCalculadora/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDaCalculadora
+{
+
+    public class Calculadora
+    {
+
+        public void PedeNumeros()
+        {
+            int valor1, valor2;
+            string NumeosD;
+
+            Console.WriteLine("Digite um número: ");
+            NumeosD = Console.ReadLine();
+            valor1 = int.Parse(NumeosD);
+
+            Console.WriteLine("Digite mais um número: ");
+            Console.ReadLine();
+            valor2 = int.Parse(NumeosD);
+
+
+
+        }
+            public int CalculaAdicao(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int CalculaSubtracao(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public int CalculaMultiplicacao(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+
+        public int CalculaDivisao(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+
+        
+    }  
+}
---
+++ UsandoaBibliotecacalculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoDaCalculadora;
+
+namespace UsandoaBibliotecacalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            calc.PedeNumeros();
+
+            Console.WriteLine(calc.CalculaAdicao(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
