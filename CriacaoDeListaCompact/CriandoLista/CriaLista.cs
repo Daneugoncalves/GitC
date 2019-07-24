244ddee71adfcec8c280cb777fecadbaa862aad9
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 11:15:09 -0300

    Cria lista compacta

---
+++ CriacaoDeListaCompact/CriandoLista/CriaLista.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeListaCompact.CriandoLista
+{
+    class CriaLista
+    {
+        string[] lista = new string[10];
+
+        public CriaLista()
+        {
+            crialista();
+            Listar();
+            LimpaLista();
+            Listar();
+        }
+
+        public void crialista()
+        {
+            for (int i = 0; i < lista.Length; i++)
+            {
+                lista[i] = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff");
+            }
+        }
+
+        public void Listar()
+        {
+            foreach (var item in lista)
+
+                Console.WriteLine(item);
+            Console.ReadKey();
+        }
+
+        public void LimpaLista()
+        {
+            for (int i = 0; i < lista.Length; i++)
+                lista[i] = string.Empty;
+
+        }
+    }
+
+}
---
+++ CriacaoDeListaCompact/Program.cs
@@ -0,0 +1,17 @@
+﻿using CriacaoDeListaCompact.CriandoLista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriacaoDeListaCompact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CriaLista = new CriaLista();
+        }
+    }
+}
