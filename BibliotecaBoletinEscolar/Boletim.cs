582f3cdeba645a0025f02f8006255c7d815668e4
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 09:42:02 -0300

    BOLETIM

---
+++ BibliotecaBoletinEscolar/Boletim.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaBoletinEscolar
+{
+    public class Boletim
+    {
+        public int CalculaMedia(int valor1, int valor2, int valor3)
+        {
+            return (valor1 + valor2 + valor3) / 3;
+        }
+
+        public int CalculaFrequencia(int Faltas, int TotalAulas)
+        {
+
+            // if (Faltas <= TotalAulas*0.25)
+            // {
+            //     frequencia = 1;
+            // }
+            //
+            // else
+            // frequencia = 0;
+
+            return (TotalAulas - Faltas) * 100 / TotalAulas;
+
+        }
+
+        public void situacao(int media, int frequecia)
+        {
+            if ((media >= 7) && (frequecia >= 75))
+            {
+                Console.WriteLine("O aluno foi Aprovado");
+            }
+
+            else
+                Console.WriteLine("O aluno foi Reprovado");
+            }
+
+        }
+
+    }
+
+
+
+
+
+
---
+++ UsandoBiblioBoletim/Program.cs
@@ -0,0 +1,31 @@
+﻿using BibliotecaBoletinEscolar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoBiblioBoletim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boletim bol = new Boletim();
+
+            Console.WriteLine("Digite a nota1");
+            var nota1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a nota2");
+            var nota2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a nota3");
+            var nota3 = Convert.ToInt32(Console.ReadLine());
+
+            var media = bol.CalculaMedia(nota1, nota2, nota3);
+            var frequencia = bol.CalculaFrequencia(100, 10);
+
+            bol.situacao(media, frequencia);
+            Console.ReadKey();
+
+        }
+    }
+}
