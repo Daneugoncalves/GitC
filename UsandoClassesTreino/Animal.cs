fd013be91e716b36e77ad805028708775bf967c5
Daniel <daniel.bnu7@gmail.com>
2019-07-28 20:06:50 -0300

    Cimit domingou

---
+++ UsandoClassesTreino/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoClassesTreino
+{
+    class Animal
+    {
+        public int codigo;   //Atributos
+        public string nome;       //Atributos
+
+        void Acordar() { }
+
+        void Comer() { }
+
+        void Dormir() { }
+
+    }
+}
---
+++ UsandoClassesTreino/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoClassesTreino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal ani = new Animal();
+
+            ani.nome = "Benji";
+            ani.codigo = 31;
+
+
+
+
+        }
+    }
+}
