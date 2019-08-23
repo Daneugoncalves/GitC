ea8a80aa10d3e4cc6a90ec25356909059eba6e43
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-23 17:21:53 -0300

    Validações

---
+++ RevisaoWebApi/Models/CustomValidFields.cs
@@ -0,0 +1,92 @@
+﻿using RevisaoWebApi.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace RevisaoWebApi.Models
+{
+    public class CustomValidFields : ValidationAttribute
+    {
+        ContextDB dB = new ContextDB();
+
+
+        private ValidFields typeField;
+
+        public CustomValidFields(ValidFields type)
+        {
+            typeField = type;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                switch (typeField)
+                {
+                    case ValidFields.ValidaLogin:
+                        return ValidaLogin(value, validationContext.DisplayName);
+                    case ValidFields.ValidaEmail:
+                        return ValidarEmail(value, validationContext.DisplayName);
+                    case ValidFields.ValidaSenha:
+                        return ValidaSenha(value, validationContext.DisplayName);
+                    case ValidFields.ValidaNome:
+                        return ValidarNome(value, validationContext.DisplayName);
+                    default:
+                        break;
+                }
+            }
+            return new ValidationResult($"O campo {validationContext.DisplayName} é obrigatório.");
+
+        }
+
+        private ValidationResult ValidarEmail(object value, string displayField)
+        {
+            bool result = Regex.IsMatch(value.ToString(), @"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+
+            if (result)
+            
+                return ValidationResult.Success;
+
+            return new ValidationResult($"O campo {displayField} é inválido.");
+
+        }
+
+        private ValidationResult ValidarNome(object value, string displayField)
+        {
+
+            bool result = Regex.IsMatch(value.ToString(), @"^((\b[A-zÀ-ú']{2,150}\b)\s*){2,}$");
+            if (result)
+                return ValidationResult.Success;
+            return new ValidationResult($"O campo {displayField} é inválido.");
+
+        }
+
+
+        private ValidationResult ValidaSenha(object value, string displayField)
+        {
+
+            bool result = Regex.IsMatch(value.ToString(), @"^.*(?=.{8,16})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#$%^&+=]).*$");
+            if (result)
+                return ValidationResult.Success;
+            return new ValidationResult($"O campo {displayField} é inválido.");
+
+        }
+
+        private ValidationResult ValidaLogin(object value, string displayField)
+        {
+
+            Usuario user = dB.usuarios.FirstOrDefault(x => x.Login == value.ToString());
+
+            if (value)
+                return ValidationResult.Success;
+            return new ValidationResult($"O campo {displayField} é inválido.");
+
+
+
+        }
+
+    }
+}
---
+++ RevisaoWebApi/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RevisaoWebApi.Models
+{
+    public class Usuario : UserControls
+    {
+       
+        [Key]
+        public int id { get; set; }
+        [CustomValidFields(Enums.ValidFields.ValidaNome)]
+        public string Nome { get; set; }
+        [CustomValidFields(Enums.ValidFields.ValidaEmail)]
+        public string Email { get; set; }
+        [CustomValidFields(Enums.ValidFields.ValidaLogin)]
+        public string Login { get; set; }
+        [CustomValidFields(Enums.ValidFields.ValidaSenha)]
+        public string Senha { get; set; }
+    }
+}
